7664205abe9a68aaa2c2adf8ddaa0a1482d18e7c
Jatin Sharma <115401137+jatin1432@users.noreply.github.com>
2025-12-08 15:24:19 +0530

    Update authController.js

--- backend/controller/authController.js
+++ backend/controller/authController.js
@@ -6,13 +6,12 @@ const registerUser = async (req, res) => {
     try {
         const { name, email, password } = req.body;
 
-        // Check if user exists
         const existingUser = await User.findOne({ email });
         if (existingUser) {
             return res.status(400).json({ message: "User already exists" });
         }
 
-        // Hash password
+
         const hashedPassword = await bcrypt.hash(password, 10);
 
         const user = await User.create({
@@ -31,15 +30,13 @@ const loginUser = async (req, res) => {
   try {
     const { email, password } = req.body;
 
-    // Select password explicitly
+
     const user = await User.findOne({ email }).select("+password");
     if (!user) return res.status(404).json({ message: "User not found" });
 
-    // Compare password
     const isMatch = await bcrypt.compare(password, user.password);
     if (!isMatch) return res.status(400).json({ message: "Invalid password" });
 
-    // Create JWT
     const token = jwt.sign(
       { id: user._id },
       process.env.JWT_SECRET,
