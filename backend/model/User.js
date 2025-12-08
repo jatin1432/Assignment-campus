8bfa55701ad5ee859247c13278123ef52634df02
Jatin Sharma <115401137+jatin1432@users.noreply.github.com>
2025-12-08 18:12:33 +0530

    Update User.js

--- backend/model/User.js
+++ backend/model/User.js
@@ -1,38 +1,15 @@
 const mongoose = require("mongoose");
-const bcrypt = require("bcrypt");
+const bcrypt = require("bcryptjs");
 
 const userSchema = new mongoose.Schema(
   {
     name: { type: String, required: true },
-
-    email: {
-      type: String,
-      required: true,
-      unique: true,
-      lowercase: true,
-    },
-
-    password: {
-      type: String,
-      select: false,
-    },
-
-    contact: { type: String },
-
+    email: { type: String, required: true, unique: true, lowercase: true },
+    password: { type: String, select: false },
     googleId: { type: String },
-
-    otp: { type: String },
-    otpExpires: { type: Date },
-
-    isVerified: { type: Boolean, default: false },
-
-    provider: {
-      type: String,
-      enum: ["email", "google", "otp"],
-      default: "email",
-    },
-
+    provider: { type: String, enum: ["email", "google"], default: "email" },
     avatar: { type: String },
+    isVerified: { type: Boolean, default: false },
   },
   { timestamps: true }
 );
@@ -40,13 +17,8 @@ const userSchema = new mongoose.Schema(
 
 userSchema.pre("save", async function (next) {
   if (!this.isModified("password")) return next();
-  try {
-    const salt = await bcrypt.genSalt(10);
-    this.password = await bcrypt.hash(this.password, salt);
-    next();
-  } catch (err) {
-    next(err);
-  }
+  if (this.password) this.password = await bcrypt.hash(this.password, 10);
+  next();
 });
 
 module.exports = mongoose.model("User", userSchema);
